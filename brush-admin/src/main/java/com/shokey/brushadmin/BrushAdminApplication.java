e8212cd8e1201d37ea2069a5f8e3316fefbb37f9
Billing <1340289667@qq.com>
2018-11-23 17:23:52 +0800

    first commit - hello

---
+++ brush-admin/src/main/java/com/shokey/brushadmin/BrushAdminApplication.java
@@ -0,0 +1,12 @@
+package com.shokey.brushadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrushAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrushAdminApplication.class, args);
+    }
+}
