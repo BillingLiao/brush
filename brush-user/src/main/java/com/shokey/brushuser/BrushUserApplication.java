26d2674a551a6ccbbe3176049d9a771e0d51c97a
Billing <1340289667@qq.com>
2018-12-04 15:26:16 +0800

    pom文件不上传

---
+++ brush-user/src/main/java/com/shokey/brushuser/BrushUserApplication.java
@@ -0,0 +1,12 @@
+package com.shokey.brushuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrushUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrushUserApplication.class, args);
+    }
+}
