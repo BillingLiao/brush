9cb1b5b63f3e9dd7295ea3b3891c626a9fbf2347
lingluoyi <329057490@qq.com>
2018-12-04 15:14:37 +0800

    添加entity

---
+++ brush-entity/src/main/java/com/shokey/brushentity/Certification.java
@@ -0,0 +1,82 @@
+package com.shokey.brushentity;
+
+
+
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+import java.io.Serializable;
+
+/**
+ * 实名认证
+ * @author Billing
+ * @date 2018/11/27 10:30
+ */
+@TableName("t_certification")
+public class Certification implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @TableId
+    private Integer readNameId;
+
+    /**
+     * 实名姓名
+     */
+    private String realName;
+
+    /**
+     * 身份证号
+     */
+    private String idNumber;
+
+    /**
+     * 身份证正面照
+     */
+    private String imgFront;
+
+    /**
+     * 身份证背面照
+     */
+    private String imgBack;
+
+    public Integer getReadNameId() {
+        return readNameId;
+    }
+
+    public void setReadNameId(Integer readNameId) {
+        this.readNameId = readNameId;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public String getImgFront() {
+        return imgFront;
+    }
+
+    public void setImgFront(String imgFront) {
+        this.imgFront = imgFront;
+    }
+
+    public String getImgBack() {
+        return imgBack;
+    }
+
+    public void setImgBack(String imgBack) {
+        this.imgBack = imgBack;
+    }
+}
