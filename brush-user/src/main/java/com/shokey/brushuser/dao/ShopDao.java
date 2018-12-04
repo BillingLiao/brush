1566ed9f6ab22ab501468fc53c8a447d91e0bb84
Billing <1340289667@qq.com>
2018-12-04 18:23:23 +0800

    添加dao文件

---
+++ brush-user/src/main/java/com/shokey/brushuser/dao/ShopDao.java
@@ -0,0 +1,8 @@
+package com.shokey.brushuser.dao;
+
+/**
+ * @author Billing
+ * @date 2018/12/4 18:13
+ */
+public interface ShopDao {
+}
---
+++ brush-user/src/main/java/com/shokey/brushuser/dao/TaskPaymentMethodDao.java
@@ -0,0 +1,11 @@
+package com.shokey.brushuser.dao;
+
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.shokey.brushuser.entity.TaskPaymentMethod;
+
+/**
+ * @author Billing
+ * @date 2018/12/4 18:17
+ */
+public interface TaskPaymentMethodDao extends BaseMapper<TaskPaymentMethod> {
+}
