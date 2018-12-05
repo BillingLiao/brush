c3fcad94a7e2eb7a69bce8d4cff11da534ad677c
lingluoyi <329057490@qq.com>
2018-12-05 18:07:26 +0800

    已经可以登陆了，账号admin，密码admin

---
+++ brush-admin/src/main/java/com/shokey/brushadmin/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.shokey.brushadmin.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+    /** logger */
+    private static final Logger log = LoggerFactory.getLogger(LoginController.class);
+
+    @RequestMapping("/login")
+    public String login(Model model){
+        return "login";
+    }
+
+}
