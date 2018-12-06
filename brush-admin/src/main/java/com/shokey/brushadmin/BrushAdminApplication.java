c9e48579eea71a56e7ce2a04f0e3d8c67504bf42
lingluoyi <329057490@qq.com>
2018-12-06 12:21:41 +0800

    开启事务注解

--- brush-admin/src/main/java/com/shokey/brushadmin/BrushAdminApplication.java
+++ brush-admin/src/main/java/com/shokey/brushadmin/BrushAdminApplication.java
@@ -3,9 +3,11 @@ package com.shokey.brushadmin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan("com.shokey.brushdao.mapper")
+@EnableTransactionManagement
 public class BrushAdminApplication {
 
     public static void main(String[] args) {
