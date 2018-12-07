1fec266cede03568885ee7c55c645a0a85c24165
lingluoyi <329057490@qq.com>
2018-12-07 14:05:07 +0800

    没啥改动

--- brush-dao/src/main/java/com/shokey/brushdao/mapper/UserMapper.java
+++ brush-dao/src/main/java/com/shokey/brushdao/mapper/UserMapper.java
@@ -9,7 +9,12 @@ import org.apache.ibatis.annotations.Options;
 @Mapper
 public interface UserMapper extends BaseMapper<User> {
 
-    @Insert("INSERT INTO `db_brush`.`t_user_role` (`role_id`, `user_id`) VALUES (2, #{phone})")
+    @Insert("INSERT INTO `db_brush`.`t_user_role` (`role_id`, `mobile`) VALUES (2, #{phone})")
     @Options(useGeneratedKeys = true, keyProperty = "id")
     int initialRole(String phone);
+
+    //还是要自己重写插入方法
+    @Insert("INSERT INTO `db_brush`.`t_user` (`user_name`, `password`, `security_password`, `salt`, `mobile`, `qq`, `status`, `registration_time`) VALUES ('NULL', #{password}, 'NULL', #{salt}, #{mobile}, 'NULL', #{status}, #{registrationTime})")
+    @Options(useGeneratedKeys = true, keyProperty = "user_id")
+    int initialUser(User user);
 }
