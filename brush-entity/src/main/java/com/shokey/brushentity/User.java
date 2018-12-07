e313eb8772c0e3d7171a516cfd9b465c4c97d1a5
lingluoyi <329057490@qq.com>
2018-12-07 14:15:24 +0800

    没啥改动

--- brush-entity/src/main/java/com/shokey/brushentity/User.java
+++ brush-entity/src/main/java/com/shokey/brushentity/User.java
@@ -1,6 +1,7 @@
 package com.shokey.brushentity;
 
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
@@ -23,7 +24,7 @@ public class User implements Serializable {
     /**
      * 用户编号
      */
-    @TableId
+    @TableId(value = "user_id",type= IdType.AUTO)
     private Integer userId;
 
     /**
