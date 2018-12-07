a1e294abec9e7038c4cc1953dc2a30d7213e0933
lingluoyi <329057490@qq.com>
2018-12-07 14:04:51 +0800

    更改sql

--- brush-dao/src/main/java/com/shokey/brushdao/mapper/RoleMapper.java
+++ brush-dao/src/main/java/com/shokey/brushdao/mapper/RoleMapper.java
@@ -3,8 +3,13 @@ package com.shokey.brushdao.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.shokey.brushentity.Role;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface RoleMapper extends BaseMapper<Role> {
 
+    @Select("select role_id, role_name, remark, create_time from t_role where role_id = (select t_user_role.role_id from t_user_role where mobile = #{mobile} limit 1)")
+    List<Role> findByMobile(String mobile);
 }
