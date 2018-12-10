af21da7c609e13a5790e9f3b41e63dd793c030f3
lingluoyi <329057490@qq.com>
2018-12-10 12:58:04 +0800

    更改为监听启动

--- brush-admin/src/main/java/com/shokey/brushadmin/BrushAdminApplication.java
+++ brush-admin/src/main/java/com/shokey/brushadmin/BrushAdminApplication.java
@@ -3,7 +3,10 @@ package com.shokey.brushadmin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 @MapperScan("com.shokey.brushdao.mapper")
@@ -11,6 +14,10 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class BrushAdminApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BrushAdminApplication.class, args);
+//        SpringApplication.run(BrushAdminApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(BrushAdminApplication.class, args);
+        DataSource dataSource = context.getBean(DataSource.class);
+
+        System.out.println(dataSource.getClass());
     }
 }
