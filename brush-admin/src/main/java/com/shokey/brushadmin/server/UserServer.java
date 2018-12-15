d8c1da2f0fcc551eee16a7d6956aa1c1a10c33fc
lingluoyi <329057490@qq.com>
2018-12-15 12:05:13 +0800

    加入账号激活（未完成）

--- brush-admin/src/main/java/com/shokey/brushadmin/server/UserServer.java
+++ brush-admin/src/main/java/com/shokey/brushadmin/server/UserServer.java
@@ -2,12 +2,14 @@ package com.shokey.brushadmin.server;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.shokey.brushadmin.black.Black2b;
+import com.shokey.brushcommon.tool.GoogleAuthenticator;
 import com.shokey.brushdao.mapper.UserMapper;
 import com.shokey.brushentity.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.transaction.Transactional;
 import java.util.Date;
@@ -21,15 +23,17 @@ public class UserServer {
     @Autowired
     private UserMapper userMapper;
 
+    //注册
+    @RequestMapping("/tikesignup")
     @Transactional(rollbackOn = Exception.class)
     public boolean Signup(String phone, String password) throws Exception {
         User user = new User();
-//        user.setUserId(2);//如果为空，则会传过去null，会报类型异常错误，脑壳痛
         user.setMobile(phone);
         String pass = Black2b.encode(64,password);
         user.setPassword(pass);
-        user.setSalt("Bright moonlight in front of bed, suspected frost on the ground.");
-        user.setStatus(0);
+        user.setSalt("Bright moonlight in front of bed, suspected frost on the ground.");//目前是固定的
+        user.setSecret(GoogleAuthenticator.generateSecretKey());//设置谷歌验证随机码
+        user.setStatus(0);//所有注册用户均是未激活用户，需要通过绑定谷歌验证器激活，为了开发方便，暂时设置为1
         user.setRegistrationTime(new Date());
         if (userMapper.selectOne(new QueryWrapper<User>().lambda().eq(User::getMobile,phone)) != null)
             throw new Exception("已经注册过了哦");
@@ -46,5 +50,9 @@ public class UserServer {
         }else {
             return false;
         }
+    }
+
+    public void Activation(String phone){
+
     }
 }
