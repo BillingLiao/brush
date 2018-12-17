5b0195c8c40faeb9b81b46cd4651c1a879b7908c
lingluoyi <329057490@qq.com>
2018-12-17 11:59:42 +0800

    放行test

--- brush-admin/src/main/java/com/shokey/brushadmin/config/CustomSecurityConfig.java
+++ brush-admin/src/main/java/com/shokey/brushadmin/config/CustomSecurityConfig.java
@@ -12,7 +12,7 @@ public class CustomSecurityConfig {
     public static final String logout_logoutUrl = "/logout";
     public static final String invalidSessionUrl = "/sessionInvalidSessionUrl";
     public static final String expiredSessionUrl = "/sessionExpiredUrl";
-    public static final String[] exclusivePaths = {"/fonts/**","/images/**","/js/**","/css/**","/vendors/**","/static/**", "/favicon.ico", "/tikesignup", "/login","/takelogin", "/401", "/404","/404","/500", "/signup", "/register.html"};
+    public static final String[] exclusivePaths = {"/fonts/**","/images/**","/js/**","/css/**","/vendors/**","/static/**", "/favicon.ico", "/tikesignup", "/login","/takelogin", "/401", "/404","/404","/500", "/signup", "/register.html","/test"};
     // cookie名字列表 退出时需要清除
     public static final String[] cookieNames = {"JSESSIONID", "gs", "g_username","g_id"};
 }
