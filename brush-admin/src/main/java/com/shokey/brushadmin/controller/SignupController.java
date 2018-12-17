52e772d9bdf30adb75ce62b7cb0fe7c9b2a91d4a
lingluoyi <329057490@qq.com>
2018-12-17 11:59:23 +0800

    完善激活相关请求

--- brush-admin/src/main/java/com/shokey/brushadmin/controller/SignupController.java
+++ brush-admin/src/main/java/com/shokey/brushadmin/controller/SignupController.java
@@ -1,12 +1,15 @@
 package com.shokey.brushadmin.controller;
 
 import com.shokey.brushadmin.server.UserServer;
+import com.shokey.brushcommon.tool.HTTPUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class SignupController {
@@ -16,14 +19,26 @@ public class SignupController {
 
     @RequestMapping("/register.html")
     public String Signup(Model model){
-        return "pages/samples/register";
+        return "pages/samples/register";//注册页面地址可以多配
     }
 
+    /**
+     * 只能使用页面注册方式，不允许ajax注册
+     * @param phone
+     * @param pass
+     * @param model
+     * @return
+     * @throws Exception
+     */
     @PostMapping("/tikesignup")
-    public String Signup(@RequestParam String phone, @RequestParam String pass, Model model) throws Exception {
-        if (userServer.Signup(phone,pass))
-            return "pages/samples/login";
-        else
+    public String Signup(HttpServletRequest request, @RequestParam String phone, @RequestParam String pass, Model model) throws Exception {
+        if (!HTTPUtils.isAjaxRequest(request)) {
+            if (userServer.Signup(phone, pass))
+                return "pages/samples/login";
+            else
+                return "pages/samples/register";
+        }else {
             return "pages/samples/register";
+        }
     }
 }
